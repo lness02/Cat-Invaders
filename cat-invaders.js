ebea8a2507f679ac534780f0227d54667ea68f40
Tiffany Hwang <tiffh2003@gmail.com>
2023-02-26 20:26:04 -0800

    Set the main scene to Cat Invaders and implemented cat's movement and bullet spawning

---
+++ cat-invaders.js
@@ -0,0 +1,158 @@
+import {defs, tiny} from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Subdivision_Sphere,
+} = tiny;
+
+// TODO: cat model goes here
+class Cat extends Shape {
+    constructor() {
+        super("position", "normal",);
+        // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
+        this.arrays.position = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1],
+            [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1],
+            [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]);
+        this.arrays.normal = Vector3.cast(
+            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
+        // Arrange the vertices into a square shape in texture space too:
+        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+    }
+}
+
+// TODO: make bullet like an actual bullet
+class Bullet extends Shape {
+    constructor() {
+        super("position", "normal");
+        this.arrays.position = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1],
+            [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1],
+            [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]);
+        this.arrays.normal = Vector3.cast(
+            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
+        // Arrange the vertices into a square shape in texture space too:
+        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+        this.bullet_x = 0;
+        this.bullet_y = 0;
+    }
+}
+
+class Base_Scene extends Scene {
+    /**
+     *  **Base_scene** is a Scene that can be added to any display canvas.
+     *  Setup the shapes, materials, camera, and lighting here.
+     */
+    constructor() {
+        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
+        super();
+        this.hover = this.swarm = false;
+        // At the beginning of our program, load one of each of these shape definitions onto the GPU.
+        this.shapes = {
+            'cat': new Cat(),
+            'bullet': new Bullet(),
+        };
+
+        // *** Materials
+        this.materials = {
+            plastic: new Material(new defs.Phong_Shader(),
+                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
+        };
+
+        // position of cat
+        this.position = 0;
+
+        // for spawning new bullets
+        this.shot = false;
+
+        // locations of current bullets
+        this.bullet_x = [];
+        this.bullet_y = [];
+    }
+
+    display(context, program_state) {
+        // display():  Called once per frame of animation. Here, the base class's display only does
+        // some initial setup.
+
+        // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+        if (!context.scratchpad.controls) {
+            //this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(Mat4.translation(0, -10, -30));
+        }
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, 1, 100);
+
+        // *** Lights: *** Values of vector or point lights.
+        const light_position = vec4(0, 5, 5, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+    }
+}
+
+export class CatInvaders extends Base_Scene {
+    /**
+     * This Scene object can be added to any display canvas.
+     * We isolate that code so it can be experimented with on its own.
+     * This gives you a very small code sandbox for editing a simple scene, and for
+     * experimenting with matrix transformations.
+     */
+
+    make_control_panel() {
+        this.key_triggered_button("Left", ['a'], () => {
+            this.position = this.position - 1;
+        });
+        this.key_triggered_button("Right", ['d'], () => {
+            this.position = this.position + 1;
+        });
+        this.key_triggered_button("Shoot", ['s'], () => {
+             this.shot = true;
+        });
+    }
+
+    draw_bullet(context, program_state, model_transform, index) {
+        model_transform = model_transform.times(Mat4.translation(this.bullet_x.at(index), this.bullet_y.at(index), 0));
+        this.shapes.bullet.draw(context, program_state, model_transform, this.materials.plastic);
+        this.bullet_y[index] = this.bullet_y.at(index) + 1;
+        return model_transform;
+    }
+
+    display(context, program_state) {
+
+        super.display(context, program_state);
+        const blue = hex_color("#1a9ffa");
+        let model_transform = Mat4.identity();
+
+        model_transform = model_transform.times(Mat4.translation(this.position, 0, 0));
+
+        // draws the cat at current position
+        this.shapes.cat.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
+
+        // if "s" has been pressed, spawn a new bullet
+        if (this.shot)
+        {
+            this.shot = false;
+            this.bullet_x.push(this.position);
+            this.bullet_y.push(0);
+        }
+
+        for (let i = 0; i<this.bullet_x.length; i++)
+        {
+            // if the bullet is too far up, remove it
+            if (this.bullet_y.at(i) > 50) // arbitrary number for top edge of screen TODO perhaps fix
+            {
+                this.bullet_x.shift();
+                this.bullet_y.shift();
+            }
+
+            // draw the bullets
+            let center = Mat4.identity();
+            center = this.draw_bullet(context, program_state, center, i);
+        }
+
+    }
+}
