1a5b1d5181689c4a62631976e9a5e0d510d1651d
Tiffany Hwang <tiffh2003@gmail.com>
2023-02-27 20:57:08 -0800

    Added skeleton for enemies and framework for testing (pausing, spawning enemies, etc.)

--- cat-invaders.js
+++ cat-invaders.js
@@ -1,7 +1,7 @@
 import {defs, tiny} from './examples/common.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Subdivision_Sphere,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
 
 // TODO: cat model goes here
@@ -43,6 +43,26 @@ class Bullet extends Shape {
     }
 }
 
+// TODO: create enemy model
+class Enemy extends Shape {
+    constructor() {
+        super("position", "normal");
+        this.arrays.position = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1],
+            [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1],
+            [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]);
+        this.arrays.normal = Vector3.cast(
+            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
+        // Arrange the vertices into a square shape in texture space too:
+        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+        this.bullet_x = 0;
+        this.bullet_y = 0;
+    }
+}
+
 class Base_Scene extends Scene {
     /**
      *  **Base_scene** is a Scene that can be added to any display canvas.
@@ -56,12 +76,15 @@ class Base_Scene extends Scene {
         this.shapes = {
             'cat': new Cat(),
             'bullet': new Bullet(),
+            'enemy': new Enemy(),
         };
 
         // *** Materials
         this.materials = {
             plastic: new Material(new defs.Phong_Shader(),
                 {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
+            bullet_material: new Material(new defs.Phong_Shader(),
+                {ambient: .4, diffusivity: .6, color: hex_color("FF0000")}),
         };
 
         // position of cat
@@ -73,6 +96,18 @@ class Base_Scene extends Scene {
         // locations of current bullets
         this.bullet_x = [];
         this.bullet_y = [];
+
+        // locations of current enemies
+        this.enemy_x = [];
+        this.enemy_y = [];
+
+        this.stopped = true;
+
+        // TODO consider using something else for timing
+        this.counter = 0;
+
+        // TODO for testing purposes
+        this.spawn = false;
     }
 
     display(context, program_state) {
@@ -104,46 +139,69 @@ export class CatInvaders extends Base_Scene {
 
     make_control_panel() {
         this.key_triggered_button("Left", ['a'], () => {
-            this.position = this.position - 1;
+            if (!this.stopped)
+                this.position = this.position - 1;
         });
         this.key_triggered_button("Right", ['d'], () => {
-            this.position = this.position + 1;
+            if (!this.stopped)
+                this.position = this.position + 1;
         });
         this.key_triggered_button("Shoot", ['s'], () => {
-             this.shot = true;
+            if (!this.stopped)
+                this.shot = true;
+        });
+        this.key_triggered_button("Pause/Unpause", [' '], () =>{
+            this.stopped = !this.stopped;
+        });
+
+        this.new_line();
+        // TODO for testing purposes
+        this.key_triggered_button("Spawn enemies [FOR TESTING PURPOSES]", ['p'], () => {
+           this.spawn = !this.spawn;
         });
     }
 
     draw_bullet(context, program_state, model_transform, index) {
         model_transform = model_transform.times(Mat4.translation(this.bullet_x.at(index), this.bullet_y.at(index), 0));
+        model_transform = model_transform.times(Mat4.scale(0.5, 0.5, 0.5)); // TODO maybe delete later, just for shrinking-cube purposes
         this.shapes.bullet.draw(context, program_state, model_transform, this.materials.plastic);
-        this.bullet_y[index] = this.bullet_y.at(index) + 1;
+        if (!this.stopped)
+            this.bullet_y[index] = this.bullet_y.at(index) + 1;
+        return model_transform;
+    }
+
+    draw_enemy(context, program_state, model_transform, index)
+    {
+        model_transform = model_transform.times(Mat4.translation(this.enemy_x.at(index), this.enemy_y.at(index), 0));
+        this.shapes.enemy.draw(context, program_state, model_transform, this.materials.bullet_material);
+        if (!this.stopped & (this.counter%30)==0) // note that the number has to be < half of the other number below
+            this.enemy_y[index] = this.enemy_y.at(index) - 1;
         return model_transform;
     }
 
     display(context, program_state) {
-
         super.display(context, program_state);
         const blue = hex_color("#1a9ffa");
         let model_transform = Mat4.identity();
+        let time = program_state.animation_time / 1000;
+        if (!this.stopped)
+            this.counter = this.counter+1;
 
         model_transform = model_transform.times(Mat4.translation(this.position, 0, 0));
 
         // draws the cat at current position
-        this.shapes.cat.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
+        this.shapes.cat.draw(context, program_state, model_transform, this.materials.plastic.override({color: blue}));
 
         // if "s" has been pressed, spawn a new bullet
-        if (this.shot)
-        {
+        if (this.shot) {
             this.shot = false;
             this.bullet_x.push(this.position);
             this.bullet_y.push(0);
         }
 
-        for (let i = 0; i<this.bullet_x.length; i++)
-        {
+        for (let i = 0; i < this.bullet_x.length; i++) {
             // if the bullet is too far up, remove it
-            if (this.bullet_y.at(i) > 50) // arbitrary number for top edge of screen TODO perhaps fix
+            if (this.bullet_y.at(i) > 30) // arbitrary number for top edge of screen TODO perhaps fix
             {
                 this.bullet_x.shift();
                 this.bullet_y.shift();
@@ -152,6 +210,29 @@ export class CatInvaders extends Base_Scene {
             // draw the bullets
             let center = Mat4.identity();
             center = this.draw_bullet(context, program_state, center, i);
+        }
+
+        if (this.spawn)
+            if ((this.counter%90)==0) {
+                // spawn enemies in a row
+                for (let i = -5; i<6; i++)
+                {
+                    this.enemy_x.push(i*3);
+                    this.enemy_y.push(20); // also arbitrary number for top edge of screen TODO fix
+                }
+            }
+
+        for (let i = 0; i<this.enemy_x.length; i++) {
+            // if the enemy is too far down, remove it
+            if (this.enemy_y.at(i) <= 0) // arbitrary number for top edge of screen TODO perhaps fix
+            {
+                this.enemy_x.shift();
+                this.enemy_y.shift();
+            }
+
+            // draw the bullets
+            let center = Mat4.identity();
+            center = this.draw_enemy(context, program_state, center, i);
         }
 
     }
