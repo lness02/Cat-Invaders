3d8488bdb82ef4217a89c3bb79105212c5e5133b
Tiffany Hwang <tiffh2003@gmail.com>
2023-03-13 08:37:20 -0700

    Revert "Added Models"
    This reverts commit 6fb51b5db458051dff1afcb848c29e6e24c2bab5.

--- cat-invaders.js
+++ cat-invaders.js
@@ -6,7 +6,23 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Texture, Square,
 } = tiny;
 
-// Objects for Satellite
+class Cube extends Shape {
+    constructor() {
+        super("position", "normal",);
+        // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
+        this.arrays.position = Vector3.cast(
+            [-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1],
+            [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1],
+            [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]);
+        this.arrays.normal = Vector3.cast(
+            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
+        // Arrange the vertices into a square shape in texture space too:
+        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+    }
+}
 class Dish extends Shape {
     constructor() {
         super("position", "normal");
@@ -104,190 +120,6 @@ class Trapizoid extends Shape {
     }
 }
 
-// Objects for UFO
-class Dome extends Shape {
-    constructor() {
-        super("position", "normal",);
-        this.arrays.position = Vector3.cast(
-            [1, 0, 4] , [3, 0, 3], [4, 0, 1], [2, 1.5, 2],
-            [-1, 0, 4] , [-3, 0, 3], [-4, 0, 1], [-2, 1.5, 2],
-            [-1, 0, -4] , [-3, 0, -3], [-4, 0, -1], [-2, 1.5, -2],
-            [1, 0, -4] , [3, 0, -3], [4, 0, -1], [2, 1.5, -2]
-        );
-        this.arrays.normal = Vector3.cast(
-            [1, 0, 4] , [3, 0, 3], [4, 0, 1], [2, 1.5, 2],
-            [-1, 0, 4] , [-3, 0, 3], [-4, 0, 1], [-2, 1.5, 2],
-            [-1, 0, -4] , [-3, 0, -3], [-4, 0, -1], [-2, 1.5, -2],
-            [1, 0, -4] , [3, 0, -3], [4, 0, -1], [2, 1.5, -2]
-        );
-        this.indices.push(
-            14,3,2,
-            3,2,1,
-            3,1,0,
-            3,0,7,
-            7,0,4,
-            7,4,5,
-            7,5,6,
-            7,6,11,
-            6,11,10,
-            11,10,9,
-            11,9,8,
-            11,8,15,
-            15,8,12,
-            15,12,13,
-            15,13,14,
-            15,14,3,
-            15,3,7,
-            15,11,7
-        );
-    }
-}
-class Dome_Cylinder extends Shape {
-    constructor() {
-        super("position", "normal",);
-        this.arrays.position = Vector3.cast(
-            [1, 1.5, 4], [3, 1.5, 3], [4, 1.5, 1],
-            [4, 1.5, -1], [3, 1.5, -3], [1, 1.5, -4],
-            [-1, 1.5, 4] , [-3, 1.5, 3], [-4, 1.5, 1],
-            [-4, 1.5, -1], [-3, 1.5, -3], [-1, 1.5, -4],
-            [1, 0, 4], [3, 0, 3], [4, 0, 1],
-            [4, 0, -1], [3, 0, -3], [1, 0, -4],
-            [-1, 0, 4] , [-3, 0, 3], [-4, 0, 1],
-            [-4, 0, -1], [-3, 0, -3], [-1, 0, -4],
-        );
-        this.arrays.normal = Vector3.cast(
-            [1, 1.5, 4], [3, 1.5, 3], [4, 1.5, 1],
-            [4, 1.5, -1], [3, 1.5, -3], [1, 1.5, -4],
-            [-1, 1.5, 4] , [-3, 1.5, 3], [-4, 1.5, 1],
-            [-4, 1.5, -1], [-3, 1.5, -3], [-1, 1.5, -4],
-            [1, 0, 4], [3, 0, 3], [4, 0, 1],
-            [4, 0, -1], [3, 0, -3], [1, 0, -4],
-            [-1, 0, 4] , [-3, 0, 3], [-4, 0, 1],
-            [-4, 0, -1], [-3, 0, -3], [-1, 0, -4],
-        );
-        this.indices.push(
-            0,12,1,
-            1,12,13,
-            1,13,2,
-            2,13,14,
-            2,14,3,
-            3,14,15,
-            3,15,4,
-            4,15,16,
-            4,16,5,
-            5,16,17,
-            5,17,11,
-            11,17,23,
-            6,18,7,
-            7,18,19,
-            7,19,8,
-            8,19,20,
-            8,20,9,
-            9,20,21,
-            9,21,10,
-            10,21,22,
-            10,22,11,
-            11,22,23,
-            6,18,0,
-            0,18,12
-        );
-    }
-}
-class UfoPlate extends Shape {
-    constructor() {
-        super("position", "normal",);
-        this.arrays.position = Vector3.cast(
-            [6, 1, 1.5], [4.5, 1, 4.5], [1.5, 1, 6],
-            [-1.5, 1, 6], [-4.5, 1, 4.5], [-6, 1, 1.5],
-            [-6, 1, -1.5], [-4.5, 1, -4.5], [-1.5, 1, -6],
-            [1.5, 1, -6], [4.5, 1, -4.5], [6, 1, -1.5],
-            [8, 0, 2], [6, 0, 6], [2, 0, 8],
-            [-2, 0, 8], [-6, 0, 6], [-8, 0, 2],
-            [-8, 0, -2], [-6, 0, -6], [-2, 0, -8],
-            [2, 0, -8], [6, 0, -6], [8, 0, -2],
-            [0,1.5,0]
-        );
-        this.arrays.normal = Vector3.cast(
-            [6, 1, 1.5], [4.5, 1, 4.5], [1.5, 1, 6],
-            [-1.5, 1, 6], [-4.5, 1, 4.5], [-6, 1, 1.5],
-            [-6, 1, -1.5], [-4.5, 1, -4.5], [-1.5, 1, -6],
-            [1.5, 1, -6], [4.5, 1, -4.5], [6, 1, -1.5],
-            [8, 0, 2], [6, 0, 6], [2, 0, 8],
-            [-2, 0, 8], [-6, 0, 6], [-8, 0, 2],
-            [-8, 0, -2], [-6, 0, -6], [-2, 0, -8],
-            [2, 0, -8], [6, 0, -6], [8, 0, -2],
-            [0,1.5,0]
-        );
-        this.indices.push(
-            1,2,24,
-            2,3,24,
-            3,4,24,
-            4,5,24,
-            5,6,24,
-            6,7,24,
-            7,8,24,
-            8,9,24,
-            9,10,24,
-            10,11,24,
-            11,0,24,
-            0,1,24,
-            0,12,1,
-            1,12,13,
-            1,13,2,
-            2,13,14,
-            2,14,3,
-            3,14,15,
-            3,15,4,
-            4,15,16,
-            4,16,5,
-            5,16,17,
-            5,17,6,
-            6,17,18,
-            6,18,7,
-            7,18,19,
-            7,19,8,
-            8,19,20,
-            8,20,9,
-            9,20,21,
-            9,21,10,
-            10,21,22,
-            10,22,11,
-            11,22,23,
-            11,23,0,
-            0,23,12
-        );
-    }
-}
-class EvilEye extends Shape {
-    constructor() {
-        super("position", "normal");
-        this.arrays.position = Vector3.cast(
-            [0,0,0], [-3,3,0], [-3,0.5,0], [-2.5,0,0],
-            [0,0,1], [-3,3,1], [-3,0.5,1], [-2.5,0,1]
-        );
-        this.arrays.normal = Vector3.cast(
-            [0,0,0], [0,3,-3], [0,0.5,-3], [0,0,2.5],
-            [0,0,1], [-3,3,1], [-3,0.5,1], [-2.5,0,1]
-        );
-
-        this.indices.push(
-            0,1,3,
-            1,2,3,
-            4,5,7,
-            5,6,7,
-            0,1,4,
-            1,4,5,
-            1,2,5,
-            2,5,6,
-            2,3,6,
-            3,6,7,
-            3,0,4,
-            3,4,7
-        );
-    }
-}
-
-
 // TODO: make bullet like an actual bullet
 class Bullet extends Shape {
     constructor() {
@@ -339,20 +171,10 @@ class Base_Scene extends Scene {
         this.hover = this.swarm = false;
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
-            cube: new defs.Cube,
-
-            plane: new defs.Square(),
-            pyramid: new Pyramid_Face(),
-            dish: new Dish(),
-            trap: new Trapizoid(),
-
-            dome: new Dome(),
-            domeCy : new Dome_Cylinder(),
-            ufo: new UfoPlate(),
-            evilEye: new EvilEye(),
-
-            rock: new defs.Subdivision_Sphere(1),
-
+            'cube': new Cube(),
+            'dish': new Dish(),
+            'pyramid': new Pyramid_Face(),
+            'trap': new Trapizoid(),
             'bullet': new Bullet(),
             'enemy': new Enemy(),
             'text': new Text_Line(35), // change text length here
@@ -362,13 +184,6 @@ class Base_Scene extends Scene {
         this.materials = {
             plastic: new Material(new defs.Phong_Shader(),
                 {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
-
-            catto: new Material(new defs.Textured_Phong, {
-                color: hex_color("#000000"),
-                ambient: 1.0,
-                texture: new Texture("assets/powell.png")
-            }),
-
             bullet_material: new Material(new defs.Phong_Shader(),
                 {ambient: .4, diffusivity: .6, color: hex_color("#FFffff")}),
             enemy_material: new Material(new defs.Phong_Shader(),
@@ -503,9 +318,7 @@ export class CatInvaders extends Base_Scene {
 
     draw_bullet(context, program_state) {
         for (let b of this.bullets) {
-            this.shapes.bullet.draw(context, program_state,
-                b.drawn_location.times(Mat4.scale(0.5, 0.5, 0.5)),
-                this.materials.bullet_material);
+            this.shapes.bullet.draw(context, program_state, b.drawn_location.times(Mat4.scale(0.5, 0.5, 0.5)), this.materials.bullet_material);
         }
     }
 
@@ -516,167 +329,53 @@ export class CatInvaders extends Base_Scene {
         }
     }
 
-    set_amogus_color() {
-        let random = Math.floor(Math.random() * 4);
-        switch(random) {
-            case 0:
-                return '#';
-            case 1:
-                return '#';
-            case 2:
-                return '#';
-            case 3:
-                return '#';
-        }
-    }
-
-    draw_amogus(context, program_state, model_transform, color) {
-        // Feet
-        this.shapes.cube.draw(context, program_state,
-            model_transform.times(Mat4.translation(-2.5,0,0))                               // Left Foot
-                .times(Mat4.scale(1.5,1.5,1.5)),
-            this.materials.plastic.override({color:hex_color(color[0])}));
-        this.shapes.cube.draw(context, program_state,                                               // Right Foot
-            model_transform.times(Mat4.translation(2.5,0,0))
-                .times(Mat4.scale(1.5,1.5,1.5)),
-            this.materials.plastic.override({color:hex_color(color[0])}));
-
-        // Body
-        this.shapes.cube.draw(context, program_state,
-            model_transform.times(Mat4.translation(0,5.5,0))
-                .times(Mat4.scale(4, 4.5, 1.5)),
-            this.materials.plastic.override({color:hex_color(color[0])}));
-
-        // Backpack
-        this.shapes.cube.draw(context, program_state,
-            model_transform.times(Mat4.translation(5.5,4.5,0))
-                .times(Mat4.scale(1.5, 3, 1)),
-            this.materials.plastic.override({color:hex_color(color[1])}));
-
-        // Visor
-        this.shapes.cube.draw(context, program_state,
-            model_transform.times(Mat4.translation(-2.5,6,0))
-                .times(Mat4.scale(2.5, 1.5, 2 )),
-            this.materials.plastic.override({color:hex_color("#9dc1d1")}));
-    }
-
-    set_rock_color() {
-        let random = Math.floor(Math.random() * 4);
-        switch(random) {
-            case 0:
-                return '#262626';
-            case 1:
-                return '#5400D7';
-            case 2:
-                return '#607BF7';
-            case 3:
-                return '#F2FDFF';
-        }
-    }
-    draw_rock(context, program_state, model_transform /*, rotation, color*/) {
-        let rock_transform = model_transform/*.rotation(rotation, 0, 1, 0)*/;
-        this.shapes.rock.draw(context, program_state, rock_transform,
-            this.materials.plastic/*.override({color:hex_color(color)})*/);
-    }
-
-    draw_ufo(context, program_state, model_transform) {
-        // Base
-        let ufo_base_transform = model_transform.times(Mat4.translation(0,-1.5,0));
-        this.shapes.ufo.draw(context, program_state,                                                // Top Plate
-            ufo_base_transform.times(Mat4.scale(.85,1.5,.85)),
-            this.materials.plastic.override({color:hex_color("#ffffff")}));
-        this.shapes.ufo.draw(context, program_state,                                                // Bottom Plate
-            ufo_base_transform.times(Mat4.scale(.85,1,.85))
-                .times(Mat4.rotation(Math.PI,1,0,0)),
-            this.materials.plastic.override({color:hex_color("#a8a8a8")}));
-
-        // Lights
-        for (let i = 0; i < 4; i = i + 1) {
-            this.shapes.cube.draw(context, program_state,
-                ufo_base_transform.times(Mat4.rotation(Math.PI * i / 4, 0, 1, 0)
-                    .times(Mat4.scale(0.5,0.5,7.2))),
-                this.materials.plastic.override({color:hex_color("#fff98f")}));
-        }
-
-        // Dome
-        let ufo_dome_transform = model_transform;
-        this.shapes.domeCy.draw(context,program_state, ufo_dome_transform,                          // Cylinder Part
-            this.materials.plastic.override({color:hex_color("#91ddf2")}));
-        this.shapes.dome.draw(context, program_state,                                               // Round Part
-            ufo_dome_transform.times(Mat4.translation(0,1.5,0)),
-            this.materials.plastic.override({color:hex_color("#91ddf2")}));
-
-        // Face
-        let ufo_face_transform = model_transform.times(Mat4.translation(0,0.5,3))
-            .times(Mat4.scale(.5,.5,1.25));
-        this.shapes.evilEye.draw(context, program_state,                                            // Left Eye
-            ufo_face_transform.times(Mat4.translation(-1,0,0)),
-            this.materials.plastic.override({color:hex_color("#ff0000")}));
-        this.shapes.evilEye.draw(context, program_state,                                            // Right Eye
-            ufo_face_transform.times(Mat4.translation(1,0,0))
-                .times(Mat4.scale(-1,1,1)),
-            this.materials.plastic.override({color:hex_color("#ff0000")}));
-    }
 
     draw_player(context, program_state, model_transform) {
         // Window
-        this.shapes.cube.draw(context, program_state,                                               // Block
-            model_transform.times(Mat4.scale(1,2,1)),
-            this.materials.plastic.override({color:hex_color("#4E67A2")}));
-        this.shapes.plane.draw(context, program_state,                                              // Cat
-            model_transform.times(Mat4.translation(0,2.01,0))
-                .times(Mat4.rotation(Math.PI/2,1,0,0))
-                .times(Mat4.rotation(Math.PI,0,0,1)),
-            this.materials.catto);
+        let center_transform = model_transform.times(Mat4.scale(1,2,1));
+        this.shapes.cube.draw(context, program_state, center_transform,
+            this.materials.plastic.override({color:hex_color("#98d2f6")}));
+        // will add cat later
 
         // Body
-        this.shapes.cube.draw(context, program_state,                                               // Top Half
-            model_transform.times(Mat4.translation(0,0,-1))
-                .times(Mat4.scale(2,1.5,1)),
-            this.materials.plastic.override({color:hex_color("#F2FDFF")}));
+        this.shapes.cube.draw(context, program_state,
+            model_transform.times(Mat4.translation(0,0,-1)).times(Mat4.scale(2,1.5,1)),
+            this.materials.plastic.override({color:hex_color("#9a9a9a")}));
 
-        this.shapes.cube.draw(context, program_state,                                               // Bottom Half
-            model_transform.times(Mat4.translation(0,0,1.5))
-                .times(Mat4.scale(2,1.5,1.5)),
-            this.materials.plastic.override({color:hex_color("#73A3D7")}));
+        this.shapes.cube.draw(context, program_state,
+            model_transform.times(Mat4.translation(0,0,1.5)).times(Mat4.scale(2,1.5,1.5)),
+            this.materials.plastic.override({color:hex_color("#767676")}));
 
         this.shapes.trap.draw(context, program_state,
-            model_transform.times(Mat4.translation(0,0,3))                                  // Engine Bit (TODO: FIX SHAPE)
+            model_transform.times(Mat4.translation(0,0,3))
                 .times(Mat4.rotation(Math.PI/2,1,0,0)),
-            this.materials.plastic.override({color:hex_color("#73A3D7")}));
+            this.materials.plastic.override({color:hex_color("#545454")}),
+        );
 
         // Dish
-        let dish_transform = model_transform.times(Mat4.translation(0,0,-2));
-        this.shapes.cube.draw(context, program_state,                                               // Dish Connector
-            dish_transform.times(Mat4.scale(0.5,0.5,0.5)),
+        this.shapes.cube.draw(context, program_state,
+            model_transform.times(Mat4.translation(0,0,-2)).times(Mat4.scale(0.5,0.5,0.5)),
             this.materials.plastic.override({color:hex_color("#545454")}));
-        dish_transform = dish_transform.times(Mat4.translation(0,0,-0.5))
-            .times(Mat4.rotation(-Math.PI/2,1,0,0));
-        this.shapes.dish.draw(context, program_state, dish_transform,                               // Dish
-            this.materials.plastic.override({color:hex_color("#D1E1FF")}));
-        this.shapes.pyramid.draw(context, program_state,                                            // Antena
-            dish_transform.times(Mat4.translation(0,0.01,0))
-                .times(Mat4.scale(1.5,2.25,1.5)),
-            this.materials.plastic.override({color:hex_color("#BCAFFA")}));
 
-        // Wings
-        this.shapes.cube.draw(context, program_state,                                               // Connectors
+        let dish_transform = model_transform.times(Mat4.translation(0,0,-2.5))
+            .times(Mat4.rotation(-Math.PI/2,1,0,0));
+        this.shapes.dish.draw(context, program_state, dish_transform,
+            this.materials.plastic.override({color:hex_color("#EDEADE")}));
+        this.shapes.pyramid.draw(context, program_state,
+            dish_transform.times(Mat4.translation(0,0.01,0)).times(Mat4.scale(1.5,2.25,1.5)),
+            this.materials.plastic.override({color:hex_color("#767676")}));
+
+        // wings
+        this.shapes.cube.draw(context, program_state,
             model_transform.times(Mat4.scale(2.5,0.25,0.5)),
             this.materials.plastic.override({color:hex_color("#545454")}));
-        for (let i = 0; i < 3; i++) {                                                               // Wing Panels
-            let wing_shift = 3.25 + 1.5 * i;
-            for ( let j = 0; j < 2; j++) {
-                this.shapes.cube.draw(context, program_state,
-                    model_transform.times(Mat4.translation(wing_shift, 0, 0.5 * (-1)** j))
-                        .times(Mat4.scale(0.75, 0.5, 0.5)),
-                    this.materials.plastic.override({color: hex_color("#4DB5F7")}));
 
-                this.shapes.cube.draw(context, program_state,
-                    model_transform.times(Mat4.translation(-wing_shift, 0, 0.5 * (-1)** j))
-                        .times(Mat4.scale(0.75, 0.5, 0.5)),
-                    this.materials.plastic.override({color: hex_color("#4DB5F7")}));
-            }
-        }
+        this.shapes.cube.draw(context, program_state,
+            model_transform.times(Mat4.translation(4.75,0,0)).times(Mat4.scale(2.5,0.5,1)),
+            this.materials.plastic.override({color:hex_color("#38386e")}));
+        this.shapes.cube.draw(context, program_state,
+            model_transform.times(Mat4.translation(-4.75,0,0,)).times(Mat4.scale(2.5,0.5,1)),
+            this.materials.plastic.override({color:hex_color("#38386e")}));
     }
 
     add_bullet(position)
